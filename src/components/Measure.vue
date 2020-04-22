f332657a564f09abec75d1e98f470481b82c9a75
haeil <dlgodlf111@naver.com>
2020-04-22 16:15:09 +0900

    add no scroll

--- src/components/Measure.vue
+++ src/components/Measure.vue
@@ -1,4 +1,4 @@
-<template>
+<template class="noscroll">
   <v-container class="fill-height px-6" fluid>
     <v-row align="center" justify="center">
       <h1>Measure</h1>
@@ -228,5 +228,11 @@ export default {
 canvas {
   max-width: 100%;
   height: auto;
+}
+.noscroll {
+  overflow: hidden;
+  position: fixed;
+  height: 100%;
+  width: 100%;
 }
 </style>
