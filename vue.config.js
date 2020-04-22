6dabe6504bf8109aa19cf19f01a6ac5551763261
haeil <dlgodlf111@naver.com>
2020-04-22 13:55:47 +0900

    change

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: ["vuetify"],
 
   publicPath: process.env.NODE_ENV === "production" ? "/SWRV_test/" : "/",
-  outputDir: "dist",
+  outputDir: "docs",
 };
